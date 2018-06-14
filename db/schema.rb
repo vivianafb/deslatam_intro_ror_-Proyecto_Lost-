4886137ed209485c53a575faaa5cc5f50e5c3939
vivianafb <vivianafb@gmail.com>
2018-06-14 18:02:25 -0400

    Link pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180612174356) do
+ActiveRecord::Schema.define(version: 20180613014012) do
 
   create_table "pins", force: :cascade do |t|
     t.string "photo"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
